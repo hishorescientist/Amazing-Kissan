53c211300250ed223fd8934f4b79b68f652d95f5
Hishore Scientist <hishore75@gmail.com>
2025-11-13 04:17:07 +0000

    completed

--- contact1.py
+++ contact1.py
@@ -3,4 +3,4 @@ import streamlit as st
 def app():
     st.title("📞 Contact Page")
     st.write("Contact Numbers :- +91 85249 46296.")
-    st.write("'                :- +91 83443 73555.")
+    st.write("Contact Numbers :- +91 83443 73555.")
